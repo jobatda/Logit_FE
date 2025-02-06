c7c20fe5ba94c35735c54f1d5b2279335c7d521d
stsr1284 <1599zzang@gmail.com>
2025-02-07 03:05:00 +0900

    fix: Layout.tsx conditional rendering, Thunder.tsx background mint banner, PostDetail.tsx fix width

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,44 +1,44 @@
-import styled, {css} from "styled-components";
-import {Outlet, useLocation} from "react-router-dom";
+import styled, { css } from "styled-components";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer.tsx";
 
 export default function Layout() {
     const location = useLocation();
-    const isWithoutHeaderFooter = location.pathname.startsWith("/Thunder/:id");
+    const isWithoutHeaderFooter = /^\/thunder\/\d+$/.test(location.pathname);
 
     return (
-        <LayoutWrapper isWithoutHeaderFooter={isWithoutHeaderFooter}>
-            <Header/>
-            <LayoutMain isWithoutHeaderFooter={isWithoutHeaderFooter}>
-                <Outlet/>
+        <LayoutWrapper $isWithoutHeaderFooter={isWithoutHeaderFooter}>
+             {!isWithoutHeaderFooter && <Header />}
+            <LayoutMain $isWithoutHeaderFooter={isWithoutHeaderFooter}>
+                <Outlet />
             </LayoutMain>
-            <Footer/>
+            {!isWithoutHeaderFooter && <Footer />}
         </LayoutWrapper>
     );
 }
 
-const LayoutWrapper = styled.div<{ isWithoutHeaderFooter: boolean }>`
-    ${({isWithoutHeaderFooter}) => isWithoutHeaderFooter
-            ? css`
-                min-height: 100vh;
-                margin: 0 auto;
-            `
-            : css`
-                position: relative;
-                min-height: 100vh;
-                padding: 56px 20px 80px;
-            `}
+const LayoutWrapper = styled.div<{ $isWithoutHeaderFooter: boolean }>`
+    ${({ $isWithoutHeaderFooter }) => $isWithoutHeaderFooter ? 
+    css`
+        min-height: 100vh;
+        margin: 0 auto;
+    ` : 
+    css`
+        position: relative;
+        min-height: 100vh;
+        padding: 56px 20px 80px;
+    `};
 `;
 
-
-const LayoutMain = styled.main<{ isWithoutHeaderFooter: boolean }>`
-    ${({isWithoutHeaderFooter}) => isWithoutHeaderFooter
-            ? css``
-            : css`
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
-                min-height: calc(100vh - 56px - 80px);
-            `}
+const LayoutMain = styled.main<{ $isWithoutHeaderFooter: boolean }>`
+    ${({ $isWithoutHeaderFooter }) => $isWithoutHeaderFooter ? 
+    css`
+    ` :
+    css`
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        min-height: calc(100vh - 56px - 80px)
+    `};
 `;
